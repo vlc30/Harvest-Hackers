6ff7f413ecedf1f073d6038bf2420a49b0f4d471
cecchettivictoria@gmail.com <>
2026-02-08 04:53:09 -0500

    Added animation for right click

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -14,7 +14,7 @@ public class LevelLoader : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0)){
+        if(Input.GetMouseButtonDown(1)){
             LoadNextLevel();
         }
     }
